93546f2dca35cf16df23d9da6e9bd437a1172d80
Rafael Santos <rafa.05125@gmail.com>
2024-01-09 22:30:40 -0300

    User

--- bank-account/src/main/java/com/rafaelsantos/bankaccount/domain/model/User.java
+++ bank-account/src/main/java/com/rafaelsantos/bankaccount/domain/model/User.java
@@ -1,2 +1,54 @@
-package com.rafaelsantos.bankaccount.domain.model;public class User {
+package com.rafaelsantos.bankaccount.domain.model;
+
+import java.util.List;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private Account account;
+    private Card card;
+    private List<Feature> features;
+    private List<News> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+
+    public List<News> getNews() {
+        return news;
+    }
 }
