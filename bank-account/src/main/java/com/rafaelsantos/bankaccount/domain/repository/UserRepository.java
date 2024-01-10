4b5acfef1e21ad1caa38defbb5af6f0fe221815d
Rafael Santos <rafa.05125@gmail.com>
2024-01-09 22:56:21 -0300

    UserRepository

--- bank-account/src/main/java/com/rafaelsantos/bankaccount/domain/repository/UserRepository.java
+++ bank-account/src/main/java/com/rafaelsantos/bankaccount/domain/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.rafaelsantos.bankaccount.domain.repository;public interface UserRepository {
+package com.rafaelsantos.bankaccount.domain.repository;
+
+import com.rafaelsantos.bankaccount.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
