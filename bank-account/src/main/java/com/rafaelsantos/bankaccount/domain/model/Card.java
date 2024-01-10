6d8dac83dd45024404f09c93fe2008c6a5127711
Rafael Santos <rafa.05125@gmail.com>
2024-01-09 22:30:05 -0300

    Card

--- bank-account/src/main/java/com/rafaelsantos/bankaccount/domain/model/Card.java
+++ bank-account/src/main/java/com/rafaelsantos/bankaccount/domain/model/Card.java
@@ -1,2 +1,34 @@
-package com.rafaelsantos.bankaccount.domain.model;public class Card {
+package com.rafaelsantos.bankaccount.domain.model;
+
+import java.math.BigDecimal;
+
+public class Card {
+
+    private Long id;
+    private String number;
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
 }
